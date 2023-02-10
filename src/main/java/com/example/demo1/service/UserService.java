00c36929436e6c2bd13c61ba15956478184b04e5
gushchinda <gushchinda@autoins.ru>
2023-02-10 21:54:22 +0300

    add transactional

--- src/main/java/com/example/demo1/service/UserService.java
+++ src/main/java/com/example/demo1/service/UserService.java
@@ -5,10 +5,12 @@ import com.example.demo1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
